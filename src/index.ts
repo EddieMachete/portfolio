b54a460ea29a8dfde3b8d39e5f7a339ec2a47a99
Eduardo <elguardianenelsendero@hotmail.com>
2023-06-09 16:38:30 +0000

    Added storybook

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import * as EMComponents from '@ui/em-design-system';
-import * as EMSections from '@ui/sections';
-import * as Pages from './app/pages';
-
-console.log(`${typeof Pages} :: ${typeof EMComponents} :: ${typeof EMSections}`);
+import '@ui/em-design-system';
+import '@ui/sections';
+import './app/pages';
